9e75c0afb416e09d238e1729e2db2ec1e80fd133
michelmoraes <michelmoraes>
2023-08-07 13:27:53 -0300

    application message start

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("run app");
+		System.out.println("running app");
 		SpringApplication.run(Application.class, args);
 	}
 
